31a171c7be73cf1eeb61362b61e69ea391bf0ccd
Dalbirsm03 <dalbirsm03@gmail.com>
2025-08-20 15:04:36 +0530

    commit

--- ML_AGENT/STATE/ML_state.py
+++ ML_AGENT/STATE/ML_state.py
@@ -1,15 +1,18 @@
 from typing_extensions import Annotated , TypedDict , Literal, Any,List,Dict
 import pandas as pd
 from typing import Union
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 
 class State(TypedDict):
     question : str
     ml_problem_type : str
-    target_column : str
-    target_series : List[pd.DataFrame]
-    target_encoded : List[pd.DataFrame]
-    target_label_encoder : str
-    target_label_mapping : str
+
+    target_column: str
+    target_encoded: Union[pd.Series, np.ndarray]
+    target_label_encoder: LabelEncoder
+    target_label_mapping: Dict[str, int]
     
     raw_data : List[pd.DataFrame]
     cleaned_data : List[pd.DataFrame]   
@@ -17,16 +20,16 @@ class State(TypedDict):
     suggestions : str
     report : str
 
-    column_types : str
+    column_types : Dict[str, List[str]]
     extracted_features : List[pd.DataFrame] 
-    feature_encoders : str
-    
+    feature_encoders: Dict[str, Union[LabelEncoder, TfidfVectorizer]]  
+
     auto_selected_features : str
     X_selected : List[pd.DataFrame] 
     selected_features : str
     ml_config : Dict[str, Any]
 
-    trained_model : str
+    trained_model : any
     metrics : Dict[str, Any]
 
     prediction_result : str
